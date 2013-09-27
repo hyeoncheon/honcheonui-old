fde2e4f94b1be704c41f959e1afc8d97cf2f0d42
Yonghwan SO <sio4@users.sf.net>
2013-09-27 16:50:41 +0900

    add comment on server model and rake db:migrate

--- honcheonui/db/migrate/20130926095256_create_servers.rb
+++ honcheonui/db/migrate/20130926095256_create_servers.rb
@@ -19,3 +19,21 @@ class CreateServers < ActiveRecord::Migration
     end
   end
 end
+
+### field information:
+# name		hostname, uname -n (eg. silver)
+# status	status of machine. (eg. ufo/...)
+# desc		user's description
+# uuid		server's uuid
+#
+# os_name	kernel name, uname -s (eg. Linux)
+# os_rel	distribution rel, lsb_release -r (eg. Ubuntu)
+# os_id		distribution id, lsb_release -i (eg. 12.04)
+# os_kernel	kernel release, uname -r (eg. 3.5.0-40-generic)
+# os_build	kernel build, uname -v (eg. #62...)
+# os_arch	kernel arch, uname -m (eg. x86_64)
+#
+# op_mode	operation mode is deployed or not. (eg. dev/prod/...)
+# op_level	operation level is similar to service level.
+#
+# st_uptime	status uptime is same as unix uptime.
